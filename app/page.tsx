51e915cf67a884aef8fd0565c2c47ca7f00b0d3a
teerakanotk <teerakan.otk@outlook.com>
2025-07-02 14:53:34 +0700

    Refactors HomePage component for consistency and clarity

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
-export default function HomePage({props}: Props){
-    return <div className="min-h-screen">HomePage</div>
+export default function HomePage() {
+  return <div className="min-h-screen">HomePage</div>;
 }
